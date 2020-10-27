43ceda76944dfb36b5348d7f7080ff8340515732
JuliaPahomova <70757343+JuliaPahomova@users.noreply.github.com>
2020-10-27 16:59:12 +0300

    init: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
+module.exports = function createDreamTeam(members) {
+  if ( !Array.isArray(members) || members.length === 0 || typeof(members)==='undefined') return false;
+  let dreamarr = [];
+  let name='';
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  for (let i=0; i<members.length; i++ ){
+    if(typeof (members[i])!=='string') continue;
+       for (let n=0; n<members[i].length; n++ )
+      {
+        if (members[i].charAt(n)===' '){
+        members[i]=members[i].replace(/\s+/g, '');
+       } else break;
+    }
+      dreamarr.push(members[i].charAt(0));
+  }
+  let bukva;
+  for (let i = 0; i < dreamarr.length; i++){
+    bukva = dreamarr[i].toUpperCase();
+    dreamarr[i] = bukva;
+  }
+
+
+  name = dreamarr.sort().join('');
+  return name;
+}
+
